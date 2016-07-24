1c4cac42f88b7290a43fd51e4b781c7ed3e9d18e
Tamas Hoffman <tms.hoffman@gmail.com>
2016-07-24 19:16:40 +0200

    Level 2 solved

--- sil2-intermediate/Player.js
+++ sil2-intermediate/Player.js
@@ -1,6 +1,20 @@
 class Player {
+  constructor(){
+    this.maxHealth = 20;
+  }
   playTurn(warrior) {
-    console.log(warrior.directionOfStairs());
-    warrior.walk(warrior.directionOfStairs());
+    let direction = warrior.directionOfStairs();
+    let space = warrior.feel(direction);
+    let health = warrior.health();
+
+    if(space.isEnemy()){
+      warrior.attack(direction);
+    } else {
+      if(health < this.maxHealth){
+        warrior.rest();
+      } else {
+        warrior.walk(direction);
+      }
+    }
   }
 }
